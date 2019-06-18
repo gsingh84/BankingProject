951b3e9873b18111b3bcef5453a2c009bfbf9054
gsingh84 <gsingh84@mail.greenriver.edu>
2019-06-17 23:29:22 -0700

    Testing

--- src/main/java/example/App.java
+++ src/main/java/example/App.java
@@ -3,7 +3,11 @@ package example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/**
+ * Entry point of my application
+ * 
+ * @author Gursimran Singh
+ */
 @SpringBootApplication
 public class App 
 {
